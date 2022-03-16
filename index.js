217f36998c8e038803611cfa80f9af4b4998296e
Tom Ho <t.vho1302@gmail.com>
2022-03-16 06:20:48 +0000

    Added content

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ const port = 3000;
 
 app.set('view engine','ejs');
 
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
   var title = 'Our Home Page';
   res.render('pages/index',{'title':title});
+})
+
+app.get('/about-artist', (req, res) => {
+  var title = 'Our About Artist Page';
+  res.render('pages/about-artist',{'title':title});
+})
+
+
+app.get('/album', (req, res) => {
+  var title = 'Our Artist Album Page';
+  res.render('pages/album',{'title':title});
+})
+app.get('/song-list', (req, res) => {
+  var title = 'Our Artist Song List Page';
+  res.render('pages/song-list',{'title':title});
 })
 
 app.listen(port, () => {

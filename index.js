a985686928c88858f702efb992d9462d624555c1
Tom Ho <t.vho1302@gmail.com>
2022-03-16 04:47:05 +0000

    added template

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine','ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World, from Express!')
+  var title = 'Our Home Page';
+  res.render('pages/index',{'title':title});
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
